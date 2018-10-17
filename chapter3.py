60ddeda2b27fba643179c9f1377b3d93685dbcf3
Paul FAllon <paulfallon92@gmail.com>
2018-10-17 17:32:53 -0400

    added 8ball stuff

--- chapter3.py
+++ chapter3.py
@@ -1,11 +1,30 @@
+import random
+
 def hello(name):
     print('Howdy!'+ name)
     print('Paul')
 
 hello('Alice')
 
+def getAnswer(answerNumber):
+    if answerNumber ==1:
+        return 'it is certain'
+    elif answerNumber == 2:
+        return 'it is decidedly so'
+    elif answerNumber == 4:
+        return 'Reply hazy try again'
+    elif answerNumber == 5:
+        return 'Ask Later'
+    elif answerNumber == 6:
+        return 'concentrate and ask again'
+    elif answerNumber -- 7:
+        return 'My reply is no'
+    elif answerNumber == 8:
+        return 'Not good'
+    elif answerNumber == 9:
+        return 'Very Doubtful'
 
-
-
-
+r = random.randint(1,9)
+fortune = getAnswer(r)
+print(fortune)
 
